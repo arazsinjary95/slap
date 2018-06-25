fd47dc0b0f62164e7420e39da99aedf4c1951dd5
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-25 19:37:55 +0300

    loggedIn interface

--- slap/Controller/ChannelVC.swift
+++ slap/Controller/ChannelVC.swift
@@ -13,6 +13,7 @@ class ChannelVC: UIViewController {
     //outlet
     //why this button is outlet the reson is because we are going to need to change the title of the loging button to user name.
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     
     //this for unwind segue and this we could get back all the way to our original vc.
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
@@ -22,11 +23,27 @@ class ChannelVC: UIViewController {
 
         //this for revealview show all except 60
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        //this for passing image and email to other viewcontroller.
+        NotificationCenter.default.addObserver(self, selector: #selector(userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            //this for changing color num to uicol
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
     }
     
 
