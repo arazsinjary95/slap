176b5afe565d8b3dc337b448b012e262c77e363f
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-29 12:04:16 +0300

    channels to tableview

--- slap/Controller/ChannelVC.swift
+++ slap/Controller/ChannelVC.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     //outlet
     //why this button is outlet the reson is because we are going to need to change the title of the loging button to user name.
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var tableView: UITableView!
     
     //this for unwind segue and this we could get back all the way to our original vc.
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         //this for revealview show all except 60
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
@@ -58,4 +62,22 @@ class ChannelVC: UIViewController {
         }
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
 }
+
