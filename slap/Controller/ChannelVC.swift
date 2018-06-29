b0032e4d381e023aa5eddcb2e2f2c14581cb61b2
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-29 21:46:56 +0300

    socket and channel fix error

--- slap/Controller/ChannelVC.swift
+++ slap/Controller/ChannelVC.swift
@@ -30,6 +30,12 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         //this for passing image and email to other viewcontroller.
         NotificationCenter.default.addObserver(self, selector: #selector(userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
+        SocketService.instance.getChannel { (success) in
+            if success {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
